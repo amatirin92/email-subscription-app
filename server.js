e59e9d51e76fe44a2a64a1c2c408595352388480
Ayanda Matiringe <amatirin92@gmail.com>
2017-12-27 00:22:11 +0000

    fixed the styling for the confirm token route

--- server.js
+++ server.js
@@ -1,147 +1,109 @@
 const express = require('express');
-const bodyParser = require(
-    'body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
 const domain = 'amatirin.me';
-const api_key =
-    'key-971866b68286f4f8713ecb2dd5ee44e9';
+const api_key = 'key-971866b68286f4f8713ecb2dd5ee44e9';
 const mailgun = require('mailgun-js')({
-    apiKey: api_key,
-    domain: domain
+    apiKey: api_key
+    , domain: domain
 });
 const MongoClient = require('mongodb').MongoClient
 var db;
-MongoClient.connect(
-    'mongodb://amatirin:ayami92@ds135946.mlab.com:35946/subscription-app',
-    (err, database) => {
-        if (err) return console.log(
-            err)
-        db = database;
-        app.listen(process.env.PORT ||
-            3000, () => {
-                console.log(
-                    'App is listening'
-                )
-            })
+MongoClient.connect('mongodb://amatirin:ayami92@ds135946.mlab.com:35946/subscription-app', (err, database) => {
+    if (err) return console.log(err)
+    db = database;
+    app.listen(process.env.PORT || 3000, () => {
+        console.log('App is listening')
     })
+})
 app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
-app.use(express.static(path.join(
-    __dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
-//index route
+    //index route
 app.get('/', (req, res) => {
-    res.sendFile(__dirname +
-        '/public/index.html'
-    )
-})
-//confirm routes
+        res.sendFile(__dirname + '/public/index.html')
+    })
+    //confirm routes
 app.get('/confirm', (req, res) => {
-    res.sendFile(__dirname +
-        '/public/confirm.html'
-    )
-})
-app.post('/confirm', (req, res) => {
-    //send message to user
-    var user = req.body;
-    user.subscribed = false;
-    //create the token
-    var chars =
-        '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var token = '';
-    for (var i = 16; i > 0; --i) {
-        token += chars[Math.round(
-            Math.random() *
-            (chars.length -
-                1))];
-    }
-    user.token = token;
-    var usertoken = user.token;
-
-    //url
-    var confirmUrl =
-        'http://the-email-subscription-app.herokuapp.com/confirm/' +
-        usertoken;
-    //message
-    var data = {
-        from: 'Admin <admin@amatirin.me>',
-        to: user.email,
-        subject: 'Please confirm',
-        text: 'Thanks for signing up with us! :) Please confirm your subscription here!  ' +
-            confirmUrl
-    };
-    mailgun.messages().send(
-        data,
-        function(error,
-            body) {});
-
-    //save user to database
-    console.log(user);
-    db.collection('subscribers')
-        .save(req.body, (err,
-            result) => {
-            if (err) return console
-                .log(
-                    err
-                )
-            res.render(
-                'confirm.ejs', {
-                    response: "Thanks! Please check your email!",
-                    title: "Confirmation"
+    res.render('confirm.ejs', {
+        title: "Confirm"
+        , response: "Confirmation page"
+    });
+    app.post('/confirm', (req, res) => {
+            //send message to user
+            var user = req.body;
+            user.subscribed = false;
+            //create the token
+            var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            var token = '';
+            for (var i = 16; i > 0; --i) {
+                token += chars[Math.round(Math.random() * (chars.length - 1))];
+            }
+            user.token = token;
+            var usertoken = user.token;
+            //url
+            var confirmUrl = 'http://the-email-subscription-app.herokuapp.com/confirm/' + usertoken;
+            //message
+            var data = {
+                from: 'Admin <admin@amatirin.me>'
+                , to: user.email
+                , subject: 'Please confirm'
+                , text: 'Thanks for signing up with us! :) Please confirm your subscription here!  ' + confirmUrl
+            };
+            mailgun.messages().send(data, function (error, body) {});
+            //save user to database
+            console.log(user);
+            db.collection('subscribers').save(req.body, (err, result) => {
+                if (err) return console.log(err)
+                res.render('confirm.ejs', {
+                    response: "Thanks! Please check your email!"
+                    , title: "Confirmation"
                 })
-        });
-})
-
-//subscribers route
-app.get('/subscribers', (req, res) => {
-    db.collection('subscribers')
-        .find().toArray((err,
-            result) => {
-            if (err) return err;
-            res.render(
-                'list.ejs', {
-                    subscribers: result,
-                    title: "List"
-                })
+            });
         })
-})
-//verify routes
-app.get('/confirm/:token', (req, res) => {
-    db.collection('subscribers')
-        .findOne({
-            token: req.params
-                .token
+        //subscribers route
+    app.get('/subscribers', (req, res) => {
+            db.collection('subscribers').find().toArray((err, result) => {
+                if (err) return err;
+                res.render('list.ejs', {
+                    subscribers: result
+                    , title: "List"
+                })
+            })
+        })
+        //verify routes
+    app.get('/confirm/:token', (req, res) => {
+        db.collection('subscribers').findOne({
+            token: req.params.token
         }, {
-            token: 1,
-            _id: 0
+            token: 1
+            , _id: 0
         }, (err, result) => {
             console.log(result)
             if (!result) {
-                res.render(
-                    'confirm.ejs', {
-                        response: "This is a bad token. Are you sure?"
-                    })
-            } else if (
-                result) {
-                db.collection(
-                    'subscribers'
-                ).update({
-                    token: req
-                        .params
-                        .token
+                res.render('confirm.ejs', {
+                    response: "This is a bad token. Are you sure?"
+                    , title: "Error"
+                })
+            }
+            else if (result) {
+                db.collection('subscribers').update({
+                    token: req.params.token
                 }, {
                     $set: {
                         subscribed: true
                     }
                 })
-                res.render(
-                    'confirm.ejs', {
-                        response: "You're all set!"
-                    })
+                res.render('confirm.ejs', {
+                    response: "You're all set!"
+                    , title: "Success"
+                })
             }
         })
+    })
 });
